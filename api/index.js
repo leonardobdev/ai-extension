9b55efe58e5d297e1d66d24696a2ee0be11337c5
leonardobdev <138508225+leonardobdev@users.noreply.github.com>
2025-06-09 16:15:00 +0000

    Fixed CORS Network

--- api/index.js
+++ api/index.js
@@ -24,6 +24,7 @@ try {
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        res.setHeader("Access-Control-Allow-Private-Network", "true");
         if (req.method == "OPTIONS") {
             res.writeHead(204, { "Content-Type": "text/plan" })
                 .end("", "utf-8");
